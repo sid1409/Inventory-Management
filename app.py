abb4c252ff9dab7a1f1abc0e548886b87846fd44
sid1409 <sid.sharma1409@gmail.com>
2022-05-05 14:54:08 -0400

    Made frontend pages and documentation

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
+#Imports
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from requests import session
 from werkzeug.security import generate_password_hash, check_password_hash
 from validators import *
 
+#Config
 app = Flask(__name__)
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3' #TODO Change DB on release
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+#Models
 class Retailer(db.Model):
     __tablename__ = 'retailer-info'
     id = db.Column(
@@ -32,13 +32,42 @@ class Retailer(db.Model):
         primary_key=False,
         unique=False,
         nullable=False
-	)
+    )
 
+class Stock(db.Model):
+    __tablename__ = 'stock'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    name = db.Column(
+        db.String(100),
+        nullable=False,
+    )
+    inv_type = db.Column(
+        db.String(100),
+        nullable=False,
+    )
+    expiry_date = db.Column(
+
+    )
+
+class Inventory(db.Model):
+    __tablename__ = 'inventory'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    name = db.Column(
+        db.String(100),
+        nullable=False,
+        unique = True
+    )
+
+#Routes
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-
 
 @app.route('/signUpShow')
 def showSignUpPage():
@@ -68,9 +97,6 @@ def signUp():
     db.session.add(retailer)
     db.session.commit()
 
-
-
-
 @app.route('/loginShow')
 def showLoginPage():
     return render_template('login.html')
@@ -84,13 +110,62 @@ def login():
     session = dict()
     session['userFound'] = True
     with db.engine.connect() as conn:
-        curr = conn.execute("SELECT password FROM 'retailer-info' WHERE name = '{}';".format(retailer_id))
+        curr = conn.execute("SELECT name, password FROM 'retailer-info' WHERE name = '{}';".format(retailer_id))
         res = curr.fetchone()
     
-    if res == None or not check_password_hash(res[0], retailer_pass):
+    if res == None or not check_password_hash(res[1], retailer_pass):
         session['userFound'] = False
         return render_template('login.html', session = session)
-    return render_template('login.html', session = session)
-    
+    else:
+        session['user'] = res[0]
+    return render_template('header.html', session = session)
+
+
+@app.route('/showAddInv')
+def showAddInv():
+    return render_template('addInv.html')
+
+
+@app.route('/addInv', methods = ['POST'])
+def addInv():
+    session = dict()
+    print(request.form['inv_type'])
+    return render_template('header.html', session = session)
+
+@app.route('/showAddPro')
+def showAddPro():
+    return render_template('addPro.html')
+
+@app.route('/addPro')
+def addPro():
+    #TODO
+    return render_template('addPro.html')
+
+@app.route('/showAltPri')
+def showAltPri():
+    return render_template('altPri.html')
+
+@app.route('/altPri')
+def alrPri():
+    #TODO
+    return render_template('addPro.html')
+
+@app.route('/showGenRep')
+def showGenRep():
+    return render_template('genReport.html')
+
+@app.route('/genReport')
+def genReport():
+    #TODO
+    return render_template()
+
+@app.route('/showSetAlert')
+def showSetAlert():
+    return render_template('setAlert.html')
+@app.route('/setAlert')
+def setAlert():
+    #TODO
+    return render_template('setAlert.html')
+
 if __name__ == "__main__":
     app.run(debug=True) #TODO remove debug on release
